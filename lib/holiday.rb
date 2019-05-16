312031cfa96e33560d53810ea1a2b44473ec88ce
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 20:19:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supplies|
-      holiday_array = holiday.split
+      holiday_array = holiday.to_s.split
       capitalized_holiday = holiday_array.collect{|holiday| holiday.capitalize}.join
       puts "#{capitalized_holiday}: #{supplies.each {|supply| puts supply}}"
     end
